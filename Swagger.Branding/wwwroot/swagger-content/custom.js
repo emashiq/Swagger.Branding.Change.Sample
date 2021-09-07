1de0abf18dc2235d02d703f16ce1182174c0e38d
Ashiqur Rahman Emran <em.ashiq@gmail.com>
2021-09-07 16:54:40 +0600

    Add project files.

---
+++ Swagger.Branding/wwwroot/swagger-content/custom.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    window.addEventListener("load", function () {
+        setTimeout(function () {
+            var logo = document.getElementsByClassName('link'); //For Changing The Link On The Logo Image
+            logo[0].href = "https://otpzone.com";
+            logo[0].target = "_blank";
+            logo[0].children[0].alt = "OTPZone_API";
+            logo[0].children[0].src = '/swagger-content/logo.png';
+            var body = document.getElementsByName('body');
+            console.log(body);
+        });
+    });
+})();
